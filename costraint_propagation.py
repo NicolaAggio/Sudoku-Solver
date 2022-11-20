3157ea10f4eed1bc32e0a88c538aaa9c900cfd98
NicolaAggio <880008@stud.unive.it>
2022-11-20 11:26:14 +0100

    Update costraint_propagation.py

--- costraint_propagation.py
+++ costraint_propagation.py
@@ -7,14 +7,14 @@ def solve_cp(board, back):
     # retrieves all the domains of the cells
     domains = getAllDomains(board)
 
-    # get the position of the most constrained cell 
+    # gets the position of the most constrained cell 
     cell = getNextMinimumDomain(domains, board)
     if cell is None:
         return (True, board, back)
     row, col = cell
     pos = row * DIMENSION + col
 
-    # for each in the cell's domain
+    # for each value in the cell's domain
     for val in domains[pos]:
 
         # if the assignment of the value is consistent with the constraint
